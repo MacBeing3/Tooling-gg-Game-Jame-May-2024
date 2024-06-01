204bd6b7b321580ddd06c69cb1d712cee6e5e66e
oreo-haru <ori.is.studying@gmail.com>
2024-06-01 08:41:21 -0400

    Delete Sprite2D.gd

--- Sprite2D.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-
-# Called when the node enters the scene tree for the first time
-
-
-func _on_child_entered_tree(node):
-	get_tree().change_scene_to_file("res://gui/level_select_screen.tscn")
