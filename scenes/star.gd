74de7887c8dd8fe5a0bc9d5239be46ceaf445ca6
oreo-haru <ori.is.studying@gmail.com>
2024-06-01 17:07:42 -0400

    update

--- scenes/star.gd
+++ scenes/star.gd
@@ -1,15 +1,13 @@
 extends Area2D
 
+@export var player_marble:RigidBody2D
 
 func _ready():
 	body_entered.connect(on_player_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func on_player_entered(body):
+func on_player_entered():
 	Transitions.transition()
 	await Transitions.on_transition_finished
 	get_tree().change_scene_to_file("res://gui/level_select_screen.tscn")
